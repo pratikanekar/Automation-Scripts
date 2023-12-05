a6c893bd55626367e2ee826478d6591266529279
pratikanekar <pratikpravinanekar@gmail.com>
2023-12-05 12:39:22 +0530

    #modified code of cronjob automation

--- add_cronjob_for_gw.py
+++ add_cronjob_for_gw.py
@@ -1,17 +1,34 @@
 from fabric import Connection
 from time import sleep
+import subprocess
+import pandas as pd
+from subprocess import TimeoutExpired
 
 hostlist = ['10.129.2.139']
 username = 'pi'
 password = 'iamgw_#@!'
 cron_job = '05 00 * * * sudo reboot'
+output_file = 'gw_cronjob_results.xlsx'
+
+# Create an empty DataFrame to store results
+result_df = pd.DataFrame(columns=['IP Address', 'Cronjob Added'])
 
 try:
     for hostname in hostlist:
+        # Check if the gateway is pingable
+        try:
+            subprocess.run(['ping', '-c', '1', hostname], check=True, timeout=2)
+        except TimeoutExpired:
+            print(f"Gateway {hostname} is not reachable. Skipping.")
+            # Add a row to the DataFrame with IP and Cronjob Added status as False
+            result_df = pd.concat([result_df, pd.DataFrame({'IP Address': [hostname], 'Cronjob Added': [False]})], ignore_index=True)
+            continue
+
         with Connection(
                 host=hostname,
                 user=username,
-                connect_kwargs={"password": password}
+                connect_kwargs={"password": password},
+                connect_timeout=2  # Set the timeout for the connection attempt
         ) as c:
             print(f"Adding cron job to {hostname}: {cron_job}")
 
@@ -19,7 +36,15 @@ try:
             c.run(f"(crontab -l ; echo '{cron_job}') | sort | uniq | crontab -", hide=True)
 
             print(f"Cron job added successfully to {hostname}")
+
+            # Add a row to the DataFrame with IP and Cronjob Added status as True
+            result_df = pd.concat([result_df, pd.DataFrame({'IP Address': [hostname], 'Cronjob Added': [True]})], ignore_index=True)
+
         sleep(0.5)
 
 except Exception as e:
     print(f"Error: {e}")
+
+# Save the DataFrame to an Excel file
+result_df.to_excel(f"download/{output_file}", index=False)
+print(f"Results saved to {output_file}")
